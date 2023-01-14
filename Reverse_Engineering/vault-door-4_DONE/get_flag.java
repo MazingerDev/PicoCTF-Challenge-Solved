48e2f57543460ef759569af223603a2c5e15c5b2
MazingerDev <mazinasd7@gmail.com>
2023-01-14 03:00:20 +0200

    vault-door-4

---
+++ Reverse_Engineering/vault-door-4_DONE/get_flag.java
@@ -0,0 +1,12 @@
+
+public class get_flag {
+
+    public static void main(String[] args) {
+        char[] flag  = {106, 85, 53, 116, 95, 52, 95, 98, 85, 110, 67, 104, 95, 48, 102, 95, 98, 89, 116, 51, 115, 95, 99, 49, 57, 52, 102, 55, 52, 53, 56, 101};
+        System.out.print("picoCTF{");
+        System.out.print(flag);
+        System.out.print("}");
+    }
+
+
+}
---
+++ Reverse_Engineering/vault-door-4_DONE/get_flag.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ java get_flag | java VaultDoor4 
